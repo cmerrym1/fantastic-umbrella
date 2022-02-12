c2e88b9f14cb357f05821413ab78c09ae050a719
Cheri M <cmerrym1@kent.edu>
2022-02-12 14:12:57 -0500

    why doesn't this work?

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
-const db = require('./db/connection');
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,9 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-db.connect(err => {
-  if (err) throw err;
-  console.log('Database connected.');
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
